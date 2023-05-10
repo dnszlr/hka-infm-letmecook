c7d0098a4f99c7e2b4bdfcde2af5cb99f2e08e93
dns <dennis.zhukki@gmail.com>
2023-05-10 19:12:57 +0200

    + setup properties files + added test print - removed database dependency

--- src/main/java/com/zeller/letmecook/LetmecookApplication.java
+++ src/main/java/com/zeller/letmecook/LetmecookApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HkaInfmLetmecookApplication {
+public class LetmecookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HkaInfmLetmecookApplication.class, args);
+        SpringApplication.run(LetmecookApplication.class, args);
+        System.out.println("test print");
     }
 
 }
