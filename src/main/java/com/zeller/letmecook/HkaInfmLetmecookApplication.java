9e1d4ebfd21b59d588a519a880f3c4059720ceab
dns <dennis.zhukki@gmail.com>
2023-05-10 18:59:40 +0200

    Initial commit

---
+++ src/main/java/com/zeller/letmecook/HkaInfmLetmecookApplication.java
@@ -0,0 +1,13 @@
+package com.zeller.letmecook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkaInfmLetmecookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HkaInfmLetmecookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeller/letmecook/HkaInfmLetmecookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeller.letmecook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkaInfmLetmecookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
