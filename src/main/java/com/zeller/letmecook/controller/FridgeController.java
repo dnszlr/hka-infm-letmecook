71e88f499bfcd06475228680a31db15df2ffd323
dns <dennis.zhukki@gmail.com>
2023-05-10 20:14:14 +0200

    implemented test path

--- src/main/java/com/zeller/letmecook/controller/FridgeController.java
+++ src/main/java/com/zeller/letmecook/controller/FridgeController.java
@@ -1,9 +1,16 @@
 package com.zeller.letmecook.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/fridge")
 public class FridgeController {
+
+    @GetMapping("/")
+    public String getHello() {
+        return "hello from application";
+    }
 }
+
