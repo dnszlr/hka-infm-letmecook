2d0e7d1ded8002269d7c31868482dc7049f80506
dnszlr <dennis.zhukki@gmail.com>
2023-05-18 16:28:00 +0200

    setup prometheus

--- src/main/java/com/zeller/letmecook/LetmecookApplication.java
+++ src/main/java/com/zeller/letmecook/LetmecookApplication.java
@@ -1,7 +1,15 @@
 package com.zeller.letmecook;
 
+import com.sun.net.httpserver.HttpServer;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 @SpringBootApplication
 public class LetmecookApplication {
@@ -9,5 +17,4 @@ public class LetmecookApplication {
     public static void main(String[] args) {
         SpringApplication.run(LetmecookApplication.class, args);
     }
-
 }
--- src/main/java/com/zeller/letmecook/repository/RecipeRepository.java
+++ src/main/java/com/zeller/letmecook/repository/RecipeRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 
 @Repository
 public interface RecipeRepository extends MongoRepository<Recipe, String> {
-
 	Recipe deleteRecipeById(String id);
 }
